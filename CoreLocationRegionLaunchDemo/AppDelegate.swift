e862e5f193f2d5f07a2e6c2ae7f49db7836ea1b4
David G. Young <davidgyoung@gmail.com>
2021-09-23 11:47:55 -0400

    first commit

--- CoreLocationRegionLaunchDemo/AppDelegate.swift
+++ CoreLocationRegionLaunchDemo/AppDelegate.swift
@@ -6,20 +6,83 @@
 //
 
 import UIKit
+import CoreLocation
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate, UNUserNotificationCenterDelegate {
+    var locationManager: CLLocationManager!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.sound,.alert,.badge]) { (granted, error) in
+            if granted {
+                NSLog("Notification Enabled Successfully")
+            }else{
+                NSLog("Notification Enable Error")
+            }
+        }
+        
+        locationManager = CLLocationManager()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        let constraint = CLBeaconIdentityConstraint(uuid: UUID(uuidString: "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6")!)
+        let region = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: "myregion")
+        region.notifyOnEntry = true
+        region.notifyOnExit = true
+        region.notifyEntryStateOnDisplay = true
+        locationManager.startMonitoring(for: region)
+        locationManager.startRangingBeacons(satisfying: constraint)
         return true
+    }
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+        NSLog("Ranged beacons: \(beacons.count)")
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        NSLog("locationManager error: (error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
+        NSLog("locationManager monitoring failure error: (error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        NSLog("enter")
+    }
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        NSLog("exit")
+
+    }
+
+    
+    func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
+        NSLog("didDetermineState: \(state)")
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+        var stateString = "unknown"
+        if state == .inside {
+            stateString = "inside"
+        }
+        if state == .outside {
+            stateString = "outside"
+        }
+        content.title = "Beacon Region: \(stateString)"
+        content.sound = UNNotificationSound.default
+        let request = UNNotificationRequest.init(identifier: "beaconstate", content: content, trigger: nil)
+
+        center.add(request) { (error) in
+            NSLog("Error sending notificaiton: \(error)")
+        }
     }
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        
+        
+        
+        
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
